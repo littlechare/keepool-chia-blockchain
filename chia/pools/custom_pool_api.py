4884b8b812b3d61f76f2a8c7d21467a7d43bb575
littlehow <kee_pool@163.com>
2021-06-23 16:51:15 +0800

    change pool url

--- chia/pools/custom_pool_api.py
+++ chia/pools/custom_pool_api.py
@@ -7,7 +7,7 @@ from typing import List
 def custom_load_pool_config(root_path: Path) -> List[PoolWalletConfig]:
     ret_list = []
     config = PoolWalletConfig(hexstr_to_bytes('ae4ef3b9bfe68949691281a015a9c16630fc8f66d48c19ca548fb80768791afa'),
-                              'http://172.30.67.79:8092/app/pool',
+                              'https://www.keepool.net/app/pool',
                               'c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8',
                               hexstr_to_bytes('344587cf06a39db471d2cc027504e8688a0a67cce961253500c956c73603fd58'),
                               hexstr_to_bytes('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'),
